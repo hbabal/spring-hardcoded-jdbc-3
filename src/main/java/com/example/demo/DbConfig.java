d40cf5f61542eb923b8522fa0b13ec27edb77e7d
Huseyin BABAL <huseyin.babal@aurea.com>
2017-10-06 16:52:13 +0300

    Initial Project

---
+++ src/main/java/com/example/demo/DbConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DbConfig {
+
+    private String url = "jdbc:oracle:thin:@oracle_db1:1521/test";
+    private String url1 = "jdbc:oracle:thin:@oracle_db2:1521/test";
+    private String url2 = "jdbc:oracle:thin:@oracle_db3:1521/test";
+    private String url3 = "jdbc:oracle:thin:@oracle_db4:1521/test";
+    private String url4 = "jdbc:oracle:thin:@oracle_db5:1521/test";
+    private String url5 = "jdbc:oracle:thin:@oracle_db6:1521/test";
+    private String url6 = "jdbc:oracle:thin:@oracle_db7:1521/test";
+    private String url7 = "jdbc:oracle:thin:@oracle_db8:1521/test";
+    private String url8 = "jdbc:oracle:thin:@oracle_db9:1521/test";
+    private String url9 = "jdbc:oracle:thin:@oracle_db10:1521/test";
+
+
+
+}
